213acd2833fa7f02f0aa8d20ebe3c0df1662c166
Nicholas Palomba <nicholaspalomba16@gmail.com>
2022-05-23 21:56:39 -0400

    css changes

--- src/authService/baseUrl.js
+++ src/authService/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "http://localhost:5001"
-export const baseUrl = process.env.REACT_APP_BASE_URL;
+export const baseUrl = "http://localhost:5001"
+// export const baseUrl = process.env.REACT_APP_BASE_URL;
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -9,70 +9,70 @@ import { Link } from "react-router-dom";
 // baseUrl = get(`/users/user-home`)
 
 const UserProfile = () => {
-    const [user, setUser] = React.useState({});
-    const [drinks, setDrinks] = React.useState([]);
-    // const baseUrl = baseUrl;
+  const [user, setUser] = React.useState({});
+  const [drinks, setDrinks] = React.useState([]);
+  // const baseUrl = baseUrl;
 
-    // React.useEffect(()=>{
-    //     axios
-    //     .get(`http://localhost:5001/users/user-home`)
-    //     .then((results)=>setUser(results.data))
-    //     .catch((err)=>console.log("something went wrong", err.message))
-    // }, [])
+  // React.useEffect(()=>{
+  //     axios
+  //     .get(`http://localhost:5001/users/user-home`)
+  //     .then((results)=>setUser(results.data))
+  //     .catch((err)=>console.log("something went wrong", err.message))
+  // }, [])
 
-    //GET USER PROFILE INFORMATION
-    React.useEffect(() => {
-        // const seeUser = () =>{
-        get("/users/user-home")
-            .then((results) => {
-                console.log(results.data);
-                setUser(results.data);
-            })
-            .catch((err) => {
-                console.log("Error occurred", err.message);
-            });
-        // }
-        // seeUser()
-    }, []);
+  //GET USER PROFILE INFORMATION
+  React.useEffect(() => {
+    // const seeUser = () =>{
+    get("/users/user-home")
+      .then((results) => {
+        console.log(results.data);
+        setUser(results.data);
+      })
+      .catch((err) => {
+        console.log("Error occurred", err.message);
+      });
+    // }
+    // seeUser()
+  }, []);
 
-    //GET USER'S DRINKS
-    React.useEffect(() => {
-        get("/users/user-profile/users-drinks")
-            .then((results) => {
-                console.log(results.data)
-                setDrinks(results.data);
-            })
-            .catch((error) => {
-                console.log("something went wrong loading the user's drinks", error.message)
-            })
-    }, []);
+  //GET USER'S DRINKS
+  React.useEffect(() => {
+    get("/users/user-profile/users-drinks")
+      .then((results) => {
+        console.log(results.data);
+        setDrinks(results.data);
+      })
+      .catch((error) => {
+        console.log(
+          "something went wrong loading the user's drinks",
+          error.message
+        );
+      });
+  }, []);
 
+  return (
+    <div>
+      <header className="user-profile-header">
+        <img className="profile-pic" src={user.profileImage} alt="profile" />
+        <h2>Welcome back, {user.username}</h2>
+      </header>
+      <section className="drinks-list">
+        <h4>Your drinks</h4>
+        {drinks.map((drink) => {
+          return (
+            <div className="user-drink">
+              <h5>
+                {drink.name} | <em>{drink.title}</em>
+              </h5>
 
-    return (
-        <div>
-            <header className="user-profile-header">
-                <img className="profile-pic" src={user.profileImage} alt="profile" />
-                <h2>Welcome back, {user.username}</h2>
-            </header>
-            <section className="drinks-list">
-                <h4>Your drinks</h4>
-                {drinks.map((drink) => {
-
-                    return (
-                        <div className="user-drink">
-                            <h5>{drink.name} | <em>{drink.title}</em></h5>
-                        
-                            <Link to={`/see-drink/${drink._id}`}>See Drink</Link>
-                            <Link to={`/edit-drink/${drink._id}`}>Edit drink</Link>
-                        
-                        </div>
-                    )
-                })}
-
-
-            </section>
-        </div>
-    );
+              <Link to={`/see-drink/${drink._id}`}>See Drink</Link>
+              <Link to={`/edit-drink/${drink._id}`}>Edit drink</Link>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default UserProfile;
